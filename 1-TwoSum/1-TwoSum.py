6993db390502ebe0015f8e8180336882931c40e9
Avalderrama04 <105869214+Avalderrama04@users.noreply.github.com>
2026-01-17 23:23:48 -0500

    File updated at 1/17/2026, 11:23:48 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,17 +1,18 @@
-# Last updated: 1/17/2026, 11:23:41 PM
-class Solution:
-    """
-    - use a hashmap to store nums and their indices {3:0, 4:1, 5:2, 6:3}
-    - subtract element in nums from the target: (i.e 7 - 3 = 4)
-    - we run into 4 -> have we seen its complement?: (i.e, 3)
-    - look up the complement, if it does not exist we add current number
-    - if we have seen the complement, return [hmap[complement], idx]
-
-    """
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hmap = {}
-        for idx, ele in enumerate(nums):
-            complement = target - ele
-            if complement in hmap:
-                return [hmap[complement],idx]
-            hmap[ele] = idx
+# Last updated: 1/17/2026, 11:23:48 PM
+1class Solution:
+2    """
+3    - use a hashmap to store nums and their indices {3:0, 4:1, 5:2, 6:3}
+4    - subtract element in nums from the target: (i.e 7 - 3 = 4)
+5    - we run into 4 -> have we seen its complement?: (i.e, 3)
+6    - look up the complement, if it does not exist we add current number
+7    - if we have seen the complement, return [hmap[complement], idx]
+8
+9    """
+10    def twoSum(self, nums: List[int], target: int) -> List[int]:
+11        hmap = {}
+12        for idx, ele in enumerate(nums):
+13            complement = target - ele
+14            if complement in hmap:
+15                return [hmap[complement],idx]
+16            hmap[ele] = idx
+17
